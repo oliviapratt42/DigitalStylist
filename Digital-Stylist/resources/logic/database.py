9faa66fbe630d1e06ec7f3c6fe094364fcc00723
Olivia Pratt <oliviapratt42@gmail.com>
2024-10-04 14:50:47 -0700

    Add files via upload

---
+++ Digital-Stylist/resources/logic/database.py
@@ -0,0 +1,33 @@
+"""
+import mysql.connector
+
+things to remember  
+      #select columns from table
+        #SELECT CustomerName, City, Country FROM Customers; 
+
+        #selects all columns from Customers table
+        #select * FROM Customers 
+
+        #SELECT * FROM Customers
+        #ORDER BY Country DESC;
+
+        INSERT INTO table_name (c1, c2)
+        VALUES (value1, value2)
+"""
+"""
+build table
+insert into funciton 
+delete from function 
+recall from 
+
+connection = mysql.connector.connect{
+    host= 127.0.0.1
+    port=3306
+    user='olivia_pratt'
+    password='passoword'
+    database='digital_stylist'
+}
+#CREATE DATABASE digital_stylist 
+
+C
+"""
